3b78293079873719e202734720d78182172e23f7
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2021-11-03 23:40:34 +0100

    Ändring

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-/* Open */
-//function openNav() {
- // document.getElementById("myNav").style.width = "100%";
-//}
-
-//function closeNav() {
- // document.getElementById("myNav").style.width = "0%";
-//}
-
- const buttons = document.querySelectorAll(`button`),
- menu = document.querySelector(`menu`);
+const buttons = document.querySelectorAll(`button`),
+  menu = document.querySelector(`menu`);
 
 menu.style.display = `none`;
 
-buttons[0].addEventListener(`click`, (evt) => menu.style.display = `block`);
-buttons[1].addEventListener(`click`, (evt) => menu.style.display = `none`);
+buttons[0].addEventListener(`click`, (evt) => (menu.style.display = `block`));
+buttons[1].addEventListener(`click`, (evt) => (menu.style.display = `none`));

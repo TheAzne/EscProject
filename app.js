bef3b79354c168bb9569976599fb6f411c7d4a35
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2021-11-05 20:00:49 +0100

    Menu_phone

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const buttons = document.querySelectorAll(`button`),
-  menu = document.querySelector(`menu`);
 
-menu.style.display = `none`;
 
-buttons[0].addEventListener(`click`, (evt) => (menu.style.display = `block`));
-buttons[1].addEventListener(`click`, (evt) => (menu.style.display = `none`));
+const btn1 = document.querySelector('#button1'),
+    btn2 = document.querySelector('#button2'),
+    menu = document.querySelector('#nav');
+
+menu.style.display = 'none';
+
+
+btn1.addEventListener('click', (evt) => {menu.style.display = 'block';});
+btn2.addEventListener('click', (evt) => {menu.style.display = 'none';});

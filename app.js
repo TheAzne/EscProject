b7be52d29f75900c6727f0a8503a9cab6559b57d
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2021-11-05 21:45:52 +0100

    Ändring

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
 
 const btn1 = document.querySelector('#button1'),
-    btn2 = document.querySelector('#button2'),
-    menu = document.querySelector('#nav');
+const btn2 = document.querySelector('#button2'),
+const menu = document.querySelector('#nav');
 
 menu.style.display = 'none';
-
 
 btn1.addEventListener('click', (evt) => {menu.style.display = 'block';});
 btn2.addEventListener('click', (evt) => {menu.style.display = 'none';});
